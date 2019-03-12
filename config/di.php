8420bc5efcf809786e1470e0e86741d3dc33ebf9
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-03-12 17:21:10 +0100

    add cli command "bin/oxidio-seo sitemap"

--- config/di.php
+++ config/di.php
@@ -5,10 +5,21 @@
 
 namespace Oxidio\Module;
 
+use DI;
+use fn;
+use Oxidio\Seo;
+
 return [
     ID       => 'oxidio/seo',
     TITLE    => 'oxidio/module-seo',
     URL      => 'https://github.com/oxidio/module-seo',
     AUTHOR   => 'oxidio',
     SETTINGS => [],
+
+    'cli' => DI\decorate(function(fn\Cli $cli) {
+        $cli->command('sitemap', Seo\SiteMap::class);
+        return $cli;
+    }),
+
+    Seo\SiteMap::class => DI\create(),
 ];
---
+++ src/Oxidio/Seo/SiteMap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (C) oxidio. See LICENSE file for license details.
+ */
+
+namespace Oxidio\Seo;
+
+use fn;
+
+class SiteMap
+{
+    /**
+     * Create a site map
+     *
+     * @param fn\Cli\IO $io
+     */
+    public function __invoke(fn\Cli\IO $io): void
+    {
+        $io->error('implement it');
+    }
+}
